f9e487d461ddbeafed8e22d85d161b57421d9a20
Sergey Maksimov <sergeymaksimov1993@gmail.com>
2023-09-28 22:02:31 +0300

    28.09.23

---
+++ src/test/java/org/serjmaks/algo/FloodFillTest.java
@@ -0,0 +1,15 @@
+package org.serjmaks.algo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FloodFillTest {
+
+    @Test
+    //TODO: add test for void method (see Ilya Polyakov mentoring)
+    public void run(int[][] image, int sr, int sc, int color, int newColor, int[][] expected) {
+    }
+
+
+}
