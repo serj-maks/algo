e7632e192d85470f8a368a782b698660e9860fc6
Sergey Maksimov <sergeymaksimov1993@gmail.com>
2023-10-01 15:08:37 +0300

    01.10.23

---
+++ src/main/java/org/serjmaks/algo/linked_list_cycle/ListNode.java
@@ -0,0 +1,12 @@
+package org.serjmaks.algo.linked_list_cycle;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        next = null;
+    }
+}
