dd57a671fc070cbf31433473869156e3018269a1
Sergey Maksimov <sergeymaksimov1993@gmail.com>
2023-10-06 07:00:05 +0300

    06.10.23

---
+++ src/main/java/org/serjmaks/algo/first_bad_version/VersionControl.java
@@ -0,0 +1,11 @@
+package org.serjmaks.algo.first_bad_version;
+
+public class VersionControl {
+
+    static boolean isBadVersion(int version) {
+
+        // some code
+
+        return true;
+    }
+}
