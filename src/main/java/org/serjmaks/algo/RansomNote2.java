c0e381aa6b3562fd14900aa252210cf4c551535c
Sergey Maksimov <sergeymaksimov1993@gmail.com>
2023-10-07 06:57:47 +0300

    07.10.23

---
+++ src/main/java/org/serjmaks/algo/RansomNote2.java
@@ -0,0 +1,24 @@
+package org.serjmaks.algo;
+
+import java.util.HashMap;
+
+public class RansomNote2 {
+
+    // use HashMap
+    public static boolean run(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) < 1) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
+
+        return true;
+    }
+}
